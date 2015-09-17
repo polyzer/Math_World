a8d623e15ac4a567c30ffcadecfc0c30042a1c55
polyzer <realpolyzer@yandex.ru>
2015-09-17 14:42:35 +0500

    Update

--- simple_math/simple_math.php
+++ simple_math/simple_math.php
@@ -14,7 +14,7 @@
  	</script>
  </head>
 <body>
-	<div id="full_lesson_container">
+	<div id="full_lessons_container">
 		<div id="lesson_title">
 			Алгебра
 		</div>
@@ -200,31 +200,65 @@
 		</div>
 	</div>
 <script>
+// это модуль для MathWorld.
+// Здесь будут отображаться уроки.
+var MathLessons = new function() 
+{
 
-// пробуем добавить!
+	// Классы:
+	var _User,
+		_Lesson,
+		_Lesson_Part,
+		_Lesson_Part_Unit,
+		_Lesson_Part_Unit_Data,
+		_Lesson_Part_Unit_Description,
+		_Lesson_Part_Unit_Link;
+
+	// Отображаемые, контейнеры:
+	// Здесь хранятся не только объекты, но и параметры
+	var DisplayElements = new Object();
+	this.DisplayElements["parameters"] = 
+	{
+		// свойства класс для разных создаваемых нами компонентов
+		"classes" : 
+		{
+			"Data": "data inline_divs",
+			"Description": "descrition inline_divs",
+			"Links": "links inline_divs",
+			"Unit" : "lesson_part_unit clear-fix",
+			"Part" : "lessons_part"
+		}
+	};
+	this.DisplayElements["divs"] = {
+		// самый главный div, который будет отображаться,
+		"full_lessons_container" : document.getElementById("full_lessons_container"),
+		"Parts" 
+	};
+
+
+	// пробуем добавить!
 	var unit_div = document.createElement("div");
-	unit_div.setAttribute("class", "lesson_part_unit clear-fix");
+	this.unit_div.setAttribute("class", "lesson_part_unit clear-fix");
 	var des_div = document.createElement("div");
-	des_div.setAttribute("class", "description inline_divs");
+	this.des_div.setAttribute("class", "description inline_divs");
 	var links_div = document.createElement("div");
-	links_div.setAttribute("class", "links inline_divs");
+	this.links_div.setAttribute("class", "links inline_divs");
 	var data_div = document.createElement("div");
-	data_div.setAttribute("class", "data inline_divs");
+	this.data_div.setAttribute("class", "data inline_divs");
 
 	var Parts = document.getElementsByClassName("lesson_part");
-	Parts[0].appendChild(unit_div);
-	unit_div.appendChild(data_div);
-	unit_div.appendChild(des_div);
-	unit_div.appendChild(links_div);
-	data_div.innerHTML = '<div class="math_data">$$\\lim_{x \\to \\infty} \\left(1 + \\frac{1}{n} \\right)^n = e$$</div>';
-	des_div.innerHTML = '<p>THERE ARE SOME TEXT</p>';
-	links_div.innerHTML = '&#9755;';
+	this.Parts[0].appendChild(unit_div);
+	this.unit_div.appendChild(data_div);
+	this.unit_div.appendChild(des_div);
+	this.unit_div.appendChild(links_div);
+	this.data_div.innerHTML = '<div class="math_data">$$\\lim_{x \\to \\infty} \\left(1 + \\frac{1}{n} \\right)^n = e$$</div>';
+	this.des_div.innerHTML = '<p>THERE ARE SOME TEXT</p>';
+	this.links_div.innerHTML = '&#9755;';
 
-	window.alert(data_div.innerHTML);
+	window.alert(this.data_div.innerHTML);
 //
 
-
-	var _User = function (params)
+	this._User = function (params)
 	{
 		var FullName,
 			Login,
@@ -232,19 +266,18 @@
 			IdKey;
 		if (params)
 		{
-			if(params.full_name)
-				this.FullName = params.full_name;
-			if (params.login)
-				this.Login = params.login;
-			if (params.pass)
-				this.Pass = params.pass;
-			if (params.id_key)
-				this.IdKey = params.id_key;
+			if(params.FullName)
+				this.FullName = params.FullName;
+			if (params.Login)
+				this.Login = params.Login;
+			if (params.Pass)
+				this.Pass = params.Pass;
+			if (params.IdKey)
+				this.IdKey = params.IdKey;
 		}
 	}
 
-
-	var _Lesson = function (params) 
+	this._Lesson = function (params) 
 	{
 		var IdKey,
 			Title, // название всего урока
@@ -254,38 +287,36 @@
 
 		if(params)
 		{
-			if (params.id_key)
+			if (params.IdKey)
 			{
-				this.IdKey = params.id_key;
+				this.IdKey = params.IdKey;
 			}
-			if (params.title)
+			if (params.Title)
 			{
-				this.Title = params.title;
+				this.Title = params.Title;
 			}
-			if (params.author)
+			if (params.Author)
 			{
-				this.Author = params.author;
+				this.Author = params.Author;
 			}
-			if (params.date)
+			if (params.Date)
 			{
-				this.Date = params.date;
+				this.Date = params.Date;
 			}
-			if (params.parts)
+			if (params.Parts)
 			{
-				this.PartsArray = parts;
+				this.PartsArray = Parts;
 			}			
 		} else
 		{
 			this.Title = "Новый Урок";
 			this.Author = this.user.name;
-			this.Date = new Date();
 			this.PartsArray.push(_Part_Of_Lesson({ status : "new"})); // вставляем новый урок
-
 		}
 
 	};
 
-	var _Part_Of_Lesson = function() 
+	this._Lesson_Part = function() 
 	{
 		var Title, //название раздела урока
 			IndexNumber, //порядковый номер данной части в уроке
@@ -293,7 +324,7 @@
 
 	}
 
-	var _Lesson_Part_Unit = function () 
+	this._Lesson_Part_Unit = function () 
 	{
 		var Type;
 		var Descriptions;
@@ -301,25 +332,31 @@
 		var Links;
 
 	}
-	var _Lesson_Part_Unit_Data = function() 
+	this._Lesson_Part_Unit_Data = function() 
 	{
 		var ID;
 		var Inner;
 		var UnitIndex;
 	}
-	var _Lesson_Part_Unit_Description = function ()
+	this._Lesson_Part_Unit_Description = function ()
 	{
 		var ID;
 		var Inner;
 		var UnitIndex;
 	}
-	var _Lesson_Part_Unit_Link = function () 
+	this._Lesson_Part_Unit_Link = function () 
 	{
 		var ID;
 		var Inner;
 		var UnitIndex;
 	}
 
+	// req_data - JSON объект
+	// подразумевается следующие параметры:
+	// request_text - текст запроса
+	// ready_func - функция, которая будет вызвана
+	// после выполнения запроса
+	// 
 	function doAjaxRequest(req_data)
 	{
 		var request = new XMLHttpRequest();
@@ -327,9 +364,7 @@
 		request.open("POST", "/simple_math_server_funcs.php", true);
 		request.onreadystatechange = function() 
 		{
-
-			
-
+			req_data.ready_func(request);
 		}
 		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		request.send(req_data.request_text);
@@ -347,20 +382,59 @@
 				if (propNames[i] == "element")
 					continue;
 				if (propNames[i] == "id"){
-					el[propNames[i]] = params[propNames[i]];
+					el.setAttribute(propNames[i],params[propNames[i]]);
 					continue;
 				}
 				if (propNames[i] == "class"){
-					el[propNames[i]] = params[propNames[i]];
+					el.setAttribute(propNames[i],params[propNames[i]]);
 					continue;
 				}
-				el.style[propNames[i]] = params[propNames[i]];
+				el.style.setAttribute(propNames[i], params[propNames[i]]);
 			}
+
+		}
+		return el;
+	}
+
+	// функция создает элементы Lesson,
+	// (Data, Description, Links)
+	// получаемые из AJAX
+	function createAndReturnObject(params)
+	{
+		if(params)
+		{
 
 		}
 	}
 
+	// Данная функция инициализирует новый урок.
+	// load - загрузка существующего по параметру (id)
+	// create - создание нового и сохранение
+	// Надо еще придумать режим редактирования
+	function Init(params)
+	{
+		if(params)
+		{
+			if(params.status === "load")
+			{
+				// Загружаем Урок!!!
 
+			} else if(params.status === "create")
+			{
+				//Создаем новый урок!
+			}
+		}
+	}
+
+	function getAllLessonsByParameter(params)
+	{
+		if (params)
+		{
+			
+		}
+	}
+
+};
 </script>
 
 </body>
